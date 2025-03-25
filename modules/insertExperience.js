80f63c342efa58d08d35fd2afd50cf1b99959c4f
Vilhelm Fontell <ville@fontell.se>
2025-03-25 08:08:27 +0100

    fixade animationer

--- modules/insertExperience.js
+++ modules/insertExperience.js
@@ -2,17 +2,14 @@ import { getEducation, getWorkExperience } from './apiCalls.js';
 import { createNewElement } from './createNewElement.js';
 
 export async function insertExperienceList(workExp, education) {
-   const workContainer = createNewElement('div', null, null, 'experienceContainer', document.getElementById('experienceContainer'));
+   const workContainer = createNewElement('div', null, 'workListContainer', 'experienceContainer', document.getElementById('allExperienceContainer'));
    insertExperience(workExp, workContainer, 'Arbetslivserfarenhet');
 
-   const eduContainer = createNewElement('div', null, null, 'experienceContainer', document.getElementById('experienceContainer'));
+   const eduContainer = createNewElement('div', null, 'eduListContainer', 'experienceContainer', document.getElementById('allExperienceContainer'));
    insertExperience(education, eduContainer, 'Utbildning');
 }
 
 function insertExperience(experienceList, container, title) {
-   console.log('hej');
-   console.log(experienceList);
-   console.log(container);
    createNewElement('h3', title, null, null, container);
    const expTable = createNewElement('div', null, 'expTable', null, container);
    for (const item of experienceList) {
@@ -20,13 +17,17 @@ function insertExperience(experienceList, container, title) {
       const timeCell = createNewElement('div', item.duration, null, 'timeCell', expRow);
       const arrow = createNewElement('i', null, null, 'fa-solid fa-angle-right clickArrow', expRow);
       const titleCell = createNewElement('div', item.title, null, 'titleCell', expRow);
-      const placeText = createNewElement('p', item.place, null, null, titleCell);
-      const description = createNewElement('p', item.description, null, 'hidden', titleCell);
+      const placeText = createNewElement('p', item.place, null, 'placeText', titleCell);
+      const description = createNewElement('p', item.description, null, 'hidden experienceDescription', titleCell);
 
       expRow.onclick = () => {
          description.classList.toggle('hidden');
-         arrow.classList.toggle('fa-angle-right');
-         arrow.classList.toggle('fa-angle-down');
+         if (arrow.classList.contains('rotateArrow')) {
+            arrow.classList.add('rerotateArrow');
+         } else {
+            arrow.classList.remove('rerotateArrow');
+         }
+         arrow.classList.toggle('rotateArrow');
       };
    }
 }
