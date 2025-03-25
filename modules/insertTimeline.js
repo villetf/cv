0b2099003edbae9bedf2d76a06ed20db2afccd31
Vilhelm Fontell <ville@fontell.se>
2025-03-25 08:08:10 +0100

    lade bl.a. till kommentarer

--- modules/insertTimeline.js
+++ modules/insertTimeline.js
@@ -3,7 +3,7 @@ import { createNewElement } from './createNewElement.js';
 
 
 export async function insertTimeline(workExperience, education) {
-   const experienceContainer = document.getElementById('experienceContainer');
+   const experienceContainer = document.getElementById('allExperienceContainer');
    const contentContainer = createNewElement('div', null, 'contentContainer', null, experienceContainer);
    const timelineContainer = createNewElement('div', null, 'timelineContainer', null, contentContainer);
    const yearsContainer = createNewElement('div', null, 'yearsContainer', null, timelineContainer);
@@ -17,6 +17,8 @@ export async function insertTimeline(workExperience, education) {
 
    let loopYear = startYear;
    let loopMonth = 1;
+
+   // Skapar containrar för varje månad sedan startåret
    for (let index = 1; index <= totalMonths; index++) {
       if (loopMonth > 12) {
          loopYear++;
@@ -38,6 +40,7 @@ export async function insertTimeline(workExperience, education) {
       loopMonth++;
    }
 
+   // Lägger till streck vid varje årsskifte
    const allMonthOne = document.querySelectorAll('[month="1"]');
    allMonthOne.forEach(monthElement => {
       if (monthElement.classList.contains('month')) {
@@ -49,70 +52,102 @@ export async function insertTimeline(workExperience, education) {
 
    const experiencesList = [];
 
+   // Lägger till alla jobberfarenheter i listan
    workExperience.forEach(work => {
       work.type = 'work';
       experiencesList.push(work);
    });
 
+   // Lägger till alla utbildningar i listan
    education.forEach(education => {
       education.type = 'edu';
       experiencesList.push(education);
    });
 
+
+   // Skapar ruta för varje merit och justerar bredden
    experiencesList.forEach(experience => {
+      if (new Date(experience.endTime) > new Date() || experience.endTime == 'Nuvarande') {
+         experience.endTime = `${new Date().getFullYear()}-${String(new Date().getMonth() + 1).padStart(2, '0')}`;
+      }
+
       const correctCell = document.getElementById(`${experience.startTime}-${experience.type}Holder`);
       const eventDiv = createNewElement('div', null, null, 'eventDiv', correctCell);
-      const marker = createNewElement('div', experience.title, `${experience.startTime}-marker`, `eventMarker ${experience.type}Marker`, eventDiv);
+      const marker = createNewElement('div', null, `${experience.startTime}-marker`, `eventMarker ${experience.type}Marker`, eventDiv);
+      const markerText = createNewElement('p', experience.title, null, 'markerText', marker);
       marker.onmouseenter = () => expandEvent(marker, experience);
       adjustEventWidth(experience, marker, correctCell);
+      checkOverflow(marker);
    });
 
-   // education.forEach(education => {
-   //    const correctCell = document.getElementById(`${education.startTime}-eduHolder`);
-   //    const eventDiv = createNewElement('div', null, null, 'eventDiv', correctCell);
-   //    const marker = createNewElement('div', education.title, `${education.startTime}-marker`, 'eventMarker eduMarker', eventDiv);
-   //    adjustEventWidth(education, marker, correctCell);
-   // });
-
+   // Kollar ifall meriter överlappar varandra och flyttar dem isåfall uppåt
    adjustEventPositions('work');
    adjustEventPositions('edu');
 
+   // Gör så att tidslinjen som default är skrollad längst till höger
    timelineContainer.scrollLeft = timelineContainer.scrollWidth;
 
+
+   // Vid ändring av fönsterstorlek, justera bredden på rutor
    window.onresize = () => {
-      adjustEventPositions('work');
       workExperience.forEach(work => {
          adjustEventWidth(work, document.getElementById(`${work.startTime}-marker`), document.getElementById(`${work.startTime}-workHolder`));
+         checkOverflow(document.getElementById(`${work.startTime}-marker`));
       });
       education.forEach(edu => {
          adjustEventWidth(edu, document.getElementById(`${edu.startTime}-marker`), document.getElementById(`${edu.startTime}-workHolder`));
+         checkOverflow(document.getElementById(`${edu.startTime}-marker`));
       });
    };
 }
 
+// Kolla ifall texten svämmar över, isåfall lägg till klass för att lägga till fade
+function checkOverflow(element) {
+   if (element.scrollWidth > element.clientWidth) {
+      element.querySelector('.markerText').classList.add('fadedMarkerText');
+   } else {
+      element.querySelector('.markerText').classList.remove('fadedMarkerText');
+   }
+}
+
+// Expandera event när man håller musen över
 function expandEvent(element, data) {
-   const oldInnerText = element.innerText;
    const timeout = setTimeout(() => {
       element.classList.remove('collapseEvent');
       element.classList.add('expandedEvent');
       element.style.zIndex = 5;
       element.innerText = '';
+
+      if (element.getBoundingClientRect().left + 400 > document.getElementById('timelineContainer').getBoundingClientRect().right) {
+         localStorage.setItem('oldTransform', element.style.transform);
+         const computedStyle = window.getComputedStyle(element);
+         const matrix = new DOMMatrix(computedStyle.transform);
+         const currentY = matrix.m42;
+         element.style.transform = `translate(-${(408 - element.getBoundingClientRect().width)}px, ${currentY}px)`;
+      }
       addEventDetails(element, data);
       if (element.classList.contains('eduMarker')) {
          element.style.top = '-24px';
       }
    }, 400);
    element.onmouseleave = () => {
+      if (localStorage.getItem('oldTransform')) {
+         element.style.transform = localStorage.getItem('oldTransform');
+         localStorage.removeItem('oldTransform');
+      }
       element.classList.remove('expandedEvent');
       setTimeout(() => {
          element.style.zIndex = 1;
       }, 500);
       element.classList.add('collapseEvent');
-      element.innerText = oldInnerText;
+      element.firstChild.remove();
+      const newText = createNewElement('p', data.title, null, 'markerText', element);
+      checkOverflow(element);
       clearTimeout(timeout);
    };
 }
 
+// Lägg till detaljer om event när man håller musen över
 function addEventDetails(element, data) {
    const expandedDiv = createNewElement('div', null, null, 'expandedDiv', element);
    const workCalendarDiv = createNewElement('div', null, null, 'flex', expandedDiv);
@@ -125,12 +160,16 @@ function addEventDetails(element, data) {
    const workDescription = createNewElement('p', data.description, null, null, expandedDiv);
 }
 
+// Justera eventbredd
 function adjustEventWidth(event, marker, correctCell) {
    const rawDate = new Date(event.endTime);
-   const endCell = document.getElementById(`${rawDate.getFullYear()}-${String(rawDate.getMonth() + 2).padStart(2, '0')}-workHolder`);
-   marker.style.width = `${endCell.getBoundingClientRect().left - correctCell.getBoundingClientRect().left - 2}px`;
+   const endCell = document.getElementById(`${rawDate.getFullYear()}-${String(rawDate.getMonth() + 1).padStart(2, '0')}-workHolder`);
+   if (marker) {
+      marker.style.width = `${(endCell.getBoundingClientRect().left + endCell.getBoundingClientRect().width) - correctCell.getBoundingClientRect().left - 2}px`;
+   }
 }
 
+// Justera event i höjdled ifall de överlappar
 function adjustEventPositions(type) {
    let merits;
    let unit;
@@ -163,7 +202,6 @@ function adjustEventPositions(type) {
          // Kollar ifall merit har mindre vänsterutrymme än element (merits vänsterkant är vänster om elements vänsterkant),
          // samt ifall de är på samma nivå i höjdled
          if (rect.left < element.left && rect.bottom == elementBottom) {
-            console.log('inne i ifen');
             matchingElements++;
          }
       });
