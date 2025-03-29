5a213064e6b2a8c18b9bbe82e7a9bfd362c1bacf
Vilhelm Fontell <ville@fontell.se>
2025-03-29 22:43:38 +0100

    tog bort dublettfiler

--- getLanguageColor.js
+++
@@ -1,6 +0,0 @@
-import { getGithubLangColor } from './apiCalls.js';
-
-export async function getLanguageColor(language) {
-   const result = await getGithubLangColor();
-   return result[language].color;
-}
