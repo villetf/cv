78c1f1d93dd8a1c2e5f32d174c17e4df3b1de252
Vilhelm Fontell <ville@fontell.se>
2025-04-11 17:12:33 +0200

    lade till felhantering för överstigna anrop

--- modules/apiCalls.js
+++ modules/apiCalls.js
@@ -1,6 +1,13 @@
+import { createNewElement } from './createNewElement.js';
+
 export function getGithubRepos() {
    return fetch('https://api.github.com/users/villetf/repos')
-      .then((response) => response.json())
+      .then((response) => {
+         if (response.status == 403) {
+            insertApiCallError();
+         }
+         return response.json();
+      })
       .then((data) => {
          return data.sort((a, b) => {
             return new Date(b.updated_at) - new Date(a.updated_at);
@@ -10,7 +17,12 @@ export function getGithubRepos() {
 
 export function getRepoLanguages(url) {
    return fetch(url)
-      .then(response => response.json());
+      .then(response => {
+         if (response.status == 403) {
+            insertApiCallError();
+         }
+         return response.json();
+      });
 }
 
 export function getGithubLangColor() {
@@ -26,4 +38,14 @@ export async function getWorkExperience() {
 export async function getEducation() {
    return fetch('data/education.json')
       .then(res => res.json());
+}
+
+export function insertApiCallError() {
+   if (document.getElementById('githubError')) {
+      return;
+   }
+   const title = document.querySelector('#githubSection h1');
+   const errorText = 'Du har uppnått din gräns för oautentiserade anrop mot Github (60 st/timme), och därför saknas viss info här. Om en stund kommer allt funka som det ska igen.';
+   createNewElement('p', errorText, 'githubError', null, title);
+   title.style.textAlign = 'center';
 }
